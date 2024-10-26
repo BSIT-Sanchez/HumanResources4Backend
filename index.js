d28adadaa4ded1f0e4e8ca1aff7c44f17d3955cf
BSIT-Sanchez <sanchezlando333@gmail.com>
2024-10-26 16:42:37 +0800

    added origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ["https://deploy"],
+        origin: ["https://human-resources4-backend.vercel.app"],
         methods: ['POST', 'GET'],
         credentials: true
     }
